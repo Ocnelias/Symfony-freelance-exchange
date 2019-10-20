789566ba9a19f46e7256a8d106a426527cd9b452
Dmytro Salenko <dmytro.salenko@pinxterapp.com>
2019-10-20 21:48:28 +0300

    language routing

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -34,6 +34,7 @@ class RegistrationFormType extends AbstractType
                 ])
             ->add('username')
             ->add('plainPassword', PasswordType::class, [
+                'label'=>'Password',
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
