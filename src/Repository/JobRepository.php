2cc17cbcf236b43c1996033a7a37048a746251bd
Dmytro Salenko <dmytro.salenko@pinxterapp.com>
2019-11-16 17:35:55 +0200

    icons, main categories search

--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -19,6 +19,7 @@ class JobRepository extends ServiceEntityRepository
     {
 
         $query=$this->createQueryBuilder('i')
+            ->join('i.category', 'cat')
             ->orderBy('i.id', 'DESC');
 
         $search_data['isPermanent'] ? $query->where('i.isPermanent = :isPermanentVal')->setParameter('isPermanentVal', $search_data['isPermanent']) : '';
@@ -27,7 +28,7 @@ class JobRepository extends ServiceEntityRepository
         $search_data['salaryCurrency'] ? $query->andWhere('i.salaryCurrency = :salaryCurrencyVal')->setParameter('salaryCurrencyVal', $search_data['salaryCurrency']) : '';
         $search_by_title ? $query->andWhere("i.title LIKE :titleVal")->setParameter('titleVal', '%'.$search_by_title.'%'): '';
         $search_cats ? $query->andWhere("i.category IN (:search_cats) ")->setParameter('search_cats', $search_cats): '';
-        $search_main_cats ? $query->andWhere("i.category.parentId IN (:search_main_cats) ")->setParameter('search_main_cats', $search_cats): '';
+        $search_main_cats ? $query->andWhere("cat.parentId = (:search_main_cats) ")->setParameter('search_main_cats', $search_main_cats): '';
         $query->getQuery()->execute();
 
         return $query;
@@ -43,7 +44,6 @@ class JobRepository extends ServiceEntityRepository
             ->getQuery()
             ->execute()
             ;
-
     }
 
 
